f8e17c46077adfec66d715c63dbf3ad64cd86e9c
LRusinov <lubomir202020@gmail.com>
2022-06-03 15:14:00 +0300

    c

---
+++ cmake-build-debug/CMakeFiles/HW2.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Lyubo/CLionProjects/HW/Control.cpp" "CMakeFiles/HW2.dir/Control.cpp.obj" "gcc" "CMakeFiles/HW2.dir/Control.cpp.obj.d"
+  "C:/Users/Lyubo/CLionProjects/HW/Form.cpp" "CMakeFiles/HW2.dir/Form.cpp.obj" "gcc" "CMakeFiles/HW2.dir/Form.cpp.obj.d"
+  "C:/Users/Lyubo/CLionProjects/HW/String.cpp" "CMakeFiles/HW2.dir/String.cpp.obj" "gcc" "CMakeFiles/HW2.dir/String.cpp.obj.d"
+  "C:/Users/Lyubo/CLionProjects/HW/main.cpp" "CMakeFiles/HW2.dir/main.cpp.obj" "gcc" "CMakeFiles/HW2.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/HW2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW2.dir/Control.cpp.obj"
+  "CMakeFiles/HW2.dir/Control.cpp.obj.d"
+  "CMakeFiles/HW2.dir/Form.cpp.obj"
+  "CMakeFiles/HW2.dir/Form.cpp.obj.d"
+  "CMakeFiles/HW2.dir/String.cpp.obj"
+  "CMakeFiles/HW2.dir/String.cpp.obj.d"
+  "CMakeFiles/HW2.dir/main.cpp.obj"
+  "CMakeFiles/HW2.dir/main.cpp.obj.d"
+  "HW2.exe"
+  "HW2.exe.manifest"
+  "HW2.pdb"
+  "libHW2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
